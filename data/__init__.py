60762f46d0583aaf1284e656348c10b038fb63a5
费政聪 <3300949577@qq.com>
2020-10-22 21:14:37 +0800

    Add files via upload

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,1 @@
-from .field import RawField, Merge, ImageDetectionsField, TextField
-from .dataset import COCO
-from torch.utils.data import DataLoader as TorchDataLoader
-
-class DataLoader(TorchDataLoader):
-    def __init__(self, dataset, *args, **kwargs):
-        super(DataLoader, self).__init__(dataset, *args, collate_fn=dataset.collate_fn(), **kwargs)
+from .field import RawField, ImageDetectionsField
